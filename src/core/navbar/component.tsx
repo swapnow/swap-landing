fd3b5387492469c0d3c95df96c7dadcd6f879da1
randyjap <randyjap@gmail.com>
2017-06-03 22:16:04 -0700

    remove mobile menu

--- src/core/navbar/component.tsx
+++ src/core/navbar/component.tsx
@@ -25,12 +25,6 @@ class NavBar extends React.Component<any, any> {
         <nav id="menu" className="navbar navbar-default navbar-static-top navbar-padded text-uppercase app-navbar">
           <div className="container">
             <div className="navbar-header">
-              <button type="button" className="navbar-toggle collapsed p-x-0" data-toggle="collapse" data-target="#navbar-collapse">
-                <span className="sr-only">Toggle navigation</span>
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
-              </button>
               <a className="navbar-brand" href="#">
                 <span>Swap</span>
               </a>
